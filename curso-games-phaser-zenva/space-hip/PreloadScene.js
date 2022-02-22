02cc699d88f89c948275b80f4b567fc612f79429
81784244368 <paulo.silva-junior@serpro.gov.br>
2022-02-22 18:55:39 -0300

    objetivo spacehip - preload - criar artes

--- curso-games-phaser-zenva/space-hip/PreloadScene.js
+++ curso-games-phaser-zenva/space-hip/PreloadScene.js
@@ -3,6 +3,19 @@ class PreloadScene extends Phaser.Scene{
 	  super("Preload");
 	}
   
+	/*
+	Criar artes
+	  - logo.png
+	  - preloadbar.png
+	  - sky.png   - plano de fundo [OK]
+	  - space.png - plano de fundo
+	  - rocha.png - spritesheet
+	  - player.png - spritesheet
+	  - power.png
+	  - particle.png
+	  - explosao - som
+	  - coletar  - som
+	*/
     preload(){
 		console.log("preload scene!");
         this.load.image('logo', 'logo.png');
